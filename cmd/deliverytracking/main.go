e6749f2b451f2446b7b5d6fe34049121895241bf
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 04:32:19 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/deliverytracking/main.go
@@ -0,0 +1,23 @@
+// cmd/deliverytracking/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "deliverytracking/internal/deliverytracking"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := deliverytracking.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
